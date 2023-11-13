4684831d02022ae011deead9fc071250037d96a8
Naharie <oneofthosewhoescaped@gmail.com>
2023-11-12 22:01:51 -0700

    Added quick load

--- QuickSaveKey.cs
+++ QuickSaveKey.cs
@@ -8,12 +8,15 @@ namespace QuickSaveKey
 		public override void Load()
 		{
 			QuickSave = KeybindLoader.RegisterKeybind(this, "Quick Save", Keys.F5);
+			QuickLoad = KeybindLoader.RegisterKeybind(this, "Quick Load", Keys.F6);
 		}
 		public override void Unload()
 		{
 			QuickSave = null;
+			QuickLoad = null;
 		}
 
 		public static ModKeybind QuickSave;
+		public static ModKeybind QuickLoad;
 	}
 }
--- QuickSavePlayer.cs
+++ QuickSavePlayer.cs
@@ -1,6 +1,8 @@
 using Terraria;
+using Terraria.Audio;
 using Terraria.GameInput;
 using Terraria.ID;
+using Terraria.IO;
 using Terraria.ModLoader;
 
 namespace QuickSaveKey;
@@ -9,22 +11,64 @@ public class QuickSavePlayer: ModPlayer
 {
     public override void ProcessTriggers(TriggersSet triggersSet)
     {
-        if (!QuickSaveMod.QuickSave.JustPressed) return;
-
-        Main.SaveRecent();
-        Main.SaveSettings();
-
-        if (Main.netMode == NetmodeID.SinglePlayer)
+        if (QuickSaveMod.QuickSave.JustPressed)
         {
-            WorldGen.saveToonWhilePlaying();
-            WorldGen.saveAndPlay();
+            Main.SaveRecent();
+            Main.SaveSettings();
 
-            Main.NewText("Game saved.", 0, 200, 0);
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            {
+                WorldGen.saveToonWhilePlaying();
+                WorldGen.saveAndPlay();
+
+                Main.NewText("Game saved.", 0, 200, 0);
+            }
+            else
+            {
+                WorldGen.saveToonWhilePlaying();
+                Main.NewText("Character saved.", 0, 200, 0);
+            }
         }
-        else
+
+        if (QuickSaveMod.QuickLoad.JustPressed)
         {
-            WorldGen.saveToonWhilePlaying();
-            Main.NewText("Character saved.", 0, 200, 0);
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            {
+                var loaded = Player.LoadPlayer(Main.ActivePlayerFileData.Path, Main.ActivePlayerFileData.IsCloudSave);
+
+                Main.player[Main.myPlayer] = loaded.Player;
+                Main.ActivePlayerFileData = loaded;
+                
+                WorldFile.LoadWorld(Main.ActiveWorldFileData.IsCloudSave);
+                
+                //WorldGen.playWorldCallBack((object)1);
+
+                if (Main.mapEnabled)
+                {
+                    Main.Map.Load();
+                }
+
+                //if (Main.netMode != NetmodeID.Server)
+                //{
+                    Main.sectionManager.SetAllSectionsLoaded();
+                //}
+                
+                if (Main.netMode == NetmodeID.SinglePlayer && Main.anglerWhoFinishedToday.Contains(Main.player[Main.myPlayer].name))
+                {
+                    Main.anglerQuestFinished = true;
+                }
+                
+                Main.player[Main.myPlayer].Spawn(PlayerSpawnContext.SpawningIntoWorld);
+                
+                WorldGen._lastSeed = Main.ActiveWorldFileData.Seed;
+                WorldFile.SetOngoingToTemps();
+                Main.resetClouds = true;
+                WorldGen.noMapUpdate = false;
+            }
+            else
+            {
+                Main.NewText("Quick load does not work in multiplayer due to this being a client side only mod!", 200, 0, 0);
+            }
         }
     }
 }
